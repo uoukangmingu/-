66a39d4abbfc4dff34d285026bad8bef8ffbf71a
mingu <mc12alsrn@gmail.com>
2025-09-25 17:42:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿// 클릭 시 자동 스크롤
+document.getElementById("main").addEventListener("click", () => {
+  document.getElementById("video-section").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("scroll-arrow").addEventListener("click", (e) => {
+  e.stopPropagation();
+  document.getElementById("video-section").scrollIntoView({ behavior: "smooth" });
+});
